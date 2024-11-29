7297f92c8ec84dbd17e7be237c0c7ea1b1e02d83
DeepakChandMarthala <deepakchand.marthala@gmail.com>
2024-11-28 21:48:09 -0500

    Initial commit

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='../frontend/templates')
@@ -15,3 +16,4 @@ def process():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
